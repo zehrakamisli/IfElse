2ef81ed8bbb6951273f15dde6252e8b5842188e7
Zehra Kamisli <kamsl.zehra97@gmail.com>
2022-05-06 21:54:53 +0300

    Proje Oluşturuldu

---
+++ IfElse/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int DT = DateTime.Now.Hour;
+            if(DT>=6 && DT<=12)
+            {
+                Console.WriteLine("Günaydın");
+            }
+           else if(DT>12 & DT<18)
+                {
+                Console.WriteLine("İyi Günler");
+            }
+            else
+            {
+                Console.WriteLine("İyi Geceler");
+            }
+            string sonuc = DT >= 6 && DT <= 12 ? "Günaydın" : DT > 12 & DT < 18 ? "İyi Günler" : "İyi geceler";
+            Console.WriteLine(sonuc);
+            Console.Read();
+        }
+    }
+}
